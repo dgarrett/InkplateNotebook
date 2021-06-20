78c8dbd5dac5f745b30a60834c26362e0c93bf54
Dylan Garrett <dylan.garrett@gmail.com>
2021-06-19 23:07:54 -0700

    Need some work on mcp23017 to enable waking from touch

--- InkplateNotebook.py
+++ InkplateNotebook.py
@@ -2,6 +2,11 @@ import network
 import time
 from inkplate6 import Inkplate
 import urequests
+import machine
+import esp32
+from machine import Pin
+from time import sleep
+
 from secrets import ssid, password
 
 # More info here: https://docs.micropython.org/en/latest/esp8266/tutorial/network_basics.html
@@ -40,7 +45,34 @@ def http_get(url):
 
     return res
 
+def sleep_until_touch():
+    # Try:
+    # from inkplate6 import _Inkplate
+    # _Inkplate._mcp23017...
 
+    # TODO extend mcp23017.py to allow the equivalent of:
+    # // Setup mcp interrupts
+    # display.pinModeInternal(MCP23017_INT_ADDR, display.mcpRegsInt, touchPadPin, INPUT);
+    # display.setIntOutput(1, false, false, HIGH);
+    # display.setIntPin(touchPadPin, RISING);
+    wake1 = Pin(34, mode = Pin.IN)
+
+    # // Go to sleep for TIME_TO_SLEEP seconds, but also enable wake up from gpio 34
+    # // Gpio 34 is where the mcp interrupt is connected, check
+    # // https://github.com/e-radionicacom/Inkplate-6-hardware/blob/master/Schematics%2C%20Gerber%2C%20BOM/Inkplate6%20Schematics.pdf
+    # // for more detail
+    #level parameter can be: esp32.WAKEUP_ANY_HIGH or esp32.WAKEUP_ALL_LOW
+    esp32.wake_on_ext0(pin = wake1, level = esp32.WAKEUP_ANY_HIGH)
+
+    #your main code goes here to perform a task
+
+    print('Im awake. Going to sleep in 10 seconds')
+    sleep(10)
+    print('Going to sleep now')
+    machine.deepsleep()
+
+
+print('wake reason: ', machine.wake_reason())
 # Calling functions defined above
 do_connect()
 response = http_get("http://micropython.org/ks/test.html")
